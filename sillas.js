43f9a93085b768293ede81f1b7fc479018502bba
jorgedelvillar <168043279+jorgedelvillar@users.noreply.github.com>
2024-04-24 16:43:04 -0700

    Add files via upload

---
+++ sillas.js
@@ -0,0 +1,18 @@
+function infantil(){
+    var horain=prompt("cuantas horas vas a Rentar:")
+    var to;
+    to=horain*2000;
+    alert("su total es de: $"+to)
+}
+function adulto(){
+    var hora=prompt("cuantas horas vas a Rentar:")
+    var tot;
+    tot=hora*3000;
+    alert("su total es de: $"+tot)
+}
+function Mesas(){
+    var hor=prompt("cuantas horas vas a Rentar:")
+    var t;
+    t=hor*3500;
+    alert("su total es de: $"+t)
+}
